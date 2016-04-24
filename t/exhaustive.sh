ade25c4e531ded8e61c581ce62a801e11343b435
David Phillips <dbphillipsnz@gmail.com>
2016-04-24 21:53:57 +1200

    Implement tests with expected failure

--- t/exhaustive.sh
+++ t/exhaustive.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-for infile in t/tests/*.in; do
+for infile in t/{positive,negative}/*.in; do
     base=`echo $infile | sed 's/\.in$//g'`
     testfile="$base.tmp"
 
@@ -9,10 +9,20 @@ for infile in t/tests/*.in; do
 
     ./dodo $testfile < "$base.dodo"
     ret=$?
-    if [ $ret -ne 0 ]; then
-        echo "dodo failed: test failed for $base"
-        exit 1
-    fi
+    case $infile in
+        t/positive/*)
+            if [ $ret -ne 0 ]; then
+                echo "dodo failed: test failed for $base"
+                exit 1
+            fi
+        ;;
+        t/negative/*)
+             if [ $ret -ne 1 ]; then
+                echo "dodo failed: expected failure test failed for $base"
+                exit 1
+            fi
+        ;;
+    esac
 
     diff $testfile "$base.out"
     ret=$?
